fe2510b14c13af6cc4de57ea4b533373b6192f45
qamichaelmaia <contatomichaelmaia@gmail.com>
2024-12-19 19:04:21 -0300

    update

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 
 const Stack = createStackNavigator();
 
 function HomeScreen() {
   return (
     <View>
-      <Text>Home Screen</Text>
+      <Text>Notificações</Text>
     </View>
   );
 }
@@ -23,10 +23,28 @@ function NotificationsScreen() {
 }
 
 export default function App() {
+  const handleIconPress = () => {
+    alert('Você clicou no sino!');
+    // Adicione a lógica desejada aqui, como navegar para outra tela ou executar uma ação.
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            headerTitle: () => (
+              <TouchableOpacity onPress={handleIconPress}>
+                <Image
+                  source={require('./assets/sino.png')} // Caminho para a imagem
+                  style={{ width: 30, height: 30 }} // Ajuste o tamanho da imagem
+                />
+              </TouchableOpacity>
+            ),
+          }}
+        />
         <Stack.Screen name="Notifications" component={NotificationsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
